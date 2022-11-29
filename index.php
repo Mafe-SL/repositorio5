744f442766e914c14ca0926e96152f12e6a52f18
Mafe-SL <fernanda.mariastb@gmail.com>
2022-11-29 11:32:29 -0600

    v2

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <h1>Soy el index</h1>
     <h1>Soy el index</h1>
     <h1>Soy el index modificado desde GitHub remoto</h1>
+    <h2>Esta modificación es desde el VScode local</h2>
 </body>
 </html>
